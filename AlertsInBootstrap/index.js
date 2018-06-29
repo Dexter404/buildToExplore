cd63d75d74da3c4486bcdf4236aea1ee0535b556
rahul arora <rahularora6532@gmail.com>
2018-06-29 11:13:20 +0530

    added confirm popups.

--- AlertsInBootstrap/index.js
+++ AlertsInBootstrap/index.js
@@ -11,3 +11,52 @@ window.info = function (msg) {
 window.success = function (msg) {
     myAlert.alertSuccess(msg);
 }
+
+function attachEventHandlers($element) {
+    $element.on("confirmed.bs.alert", () => {
+        $("#displayMessage").fadeIn().html("Clicked OK!");
+    })
+    .on("cancelled.bs.alert", () => {
+        $("#displayMessage").fadeIn().html("Clicked cancel.");
+    });
+}
+function confirmError(msg) {
+    $element = myAlert.confirmError(msg);
+    attachEventHandlers($element);
+
+    setTimeout(function () {
+        $("#displayMessage").fadeOut(function () {
+            $("#displayMessage").html("");
+        });
+    }, 3000);
+}
+function confirmWarning(msg) {
+    $element = myAlert.confirmWarning(msg);
+    attachEventHandlers($element);
+
+    setTimeout(function () {
+        $("#displayMessage").fadeOut(function () {
+            $("#displayMessage").html("");
+        });
+    }, 3000);
+}
+function confirmInfo(msg) {
+    $element = myAlert.confirmInfo(msg);
+    attachEventHandlers($element);
+
+    setTimeout(function () {
+        $("#displayMessage").fadeOut(function () {
+            $("#displayMessage").html("");
+        });
+    }, 3000);
+}
+function confirmSuccess(msg) {
+    $element = myAlert.confirmSuccess(msg);
+    attachEventHandlers($element);
+
+    setTimeout(function () {
+        $("#displayMessage").fadeOut(function () {
+            $("#displayMessage").html("");
+        });
+    }, 3000);
+}
--- AlertsInBootstrap/popup.js
+++ AlertsInBootstrap/popup.js
@@ -1,39 +1,57 @@
 class AppPopups {
-    __createAlertElement(msg, type) {
-        /* valid types: info, warning, error, success */
-        let alertClass = 'alert-' + type;
-        if (type == 'error')
-            alertClass = 'alert-danger';
-
+    __createPopupElement(msg, css, type) {
+        /* valid types: alert, confirm
+         * valid css: info, warning, danger, success 
+         */
+        let title = "";
         let glyphiconClass = "";
-        switch (type) {
-            case 'info':
+        switch (css) {
+            case 'info': {
+                title = "Info"
                 glyphiconClass = "glyphicon-info-sign";
                 break;
-            case 'warning':
+            }
+            case 'warning': {
+                title = "Warning";
                 glyphiconClass = "glyphicon-question-sign";
                 break;
-            case 'error':
+            }
+            case 'danger': {
+                title = "Error";
                 glyphiconClass = "glyphicon-exclamation-sign";
                 break;
-            case 'success':
+            }
+            case 'success': {
+                title = "Success";
                 glyphiconClass = "glyphicon-ok";
                 break;
+            }
         }
 
-        let dom = '<div class="top-alert">' +
-            '<div class="alert ' + alertClass + ' alert-dismissible fade in " role="alert">' +
-            '<i class="glyphicon ' + glyphiconClass + '"></i> ' +
-            msg +
-            '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
-            '<span aria-hidden="true">×</span>' +
-            '</button>' +
-            '</div>' +
-            '</div>';
+        let btnClass = 'btn-' + css;
+        let buttons = '<p class="pull-right" style="margin-right: -20px;">'
+            + '<button type="button" class="btn ' + btnClass + '" id="confirmButton">OK</button> '
+            + '<button type="button" class="btn btn-default" id="cancelButton" data-dismiss="alert">Cancel</button>'
+            + '</p>';
+
+        let alertClass = 'alert-' + css;
+        let dom = '<div class="top-alert">'
+            + '<div class="alert ' + alertClass + ' alert-dismissible fade in text-left" role="alert">'
+            + '<h4>' + '<i class="glyphicon ' + glyphiconClass + '"></i> ' + title + '</h4>'
+            + '<p>' + msg + '</p>';
+
+        if (type == 'confirm')
+            dom += buttons;
+
+        dom += '<button type="button" class="close" data-dismiss="alert" aria-label="Close">'
+            + '<span aria-hidden="true">×</span>'
+            + '</button>'
+            + '</div>'
+            + '</div>';
         return dom;
     }
     alertError(msg) {
-        let dom = this.__createAlertElement(msg, 'error');
+        let dom = this.__createPopupElement(msg, 'danger', 'alert');
         let $dom = $(dom);
         $dom.hide();
         $("body").append($dom);
@@ -45,7 +63,7 @@ class AppPopups {
         }, 6000);
     }
     alertWarning(msg) {
-        let dom = this.__createAlertElement(msg, 'warning');
+        let dom = this.__createPopupElement(msg, 'warning', 'alert');
         let $dom = $(dom);
         $dom.hide();
         $("body").append($dom);
@@ -57,7 +75,7 @@ class AppPopups {
         }, 6000);
     }
     alertInfo(msg) {
-        let dom = this.__createAlertElement(msg, 'info');
+        let dom = this.__createPopupElement(msg, 'info', 'alert');
         let $dom = $(dom);
         $dom.hide();
         $("body").append($dom);
@@ -69,7 +87,7 @@ class AppPopups {
         }, 6000);
     }
     alertSuccess(msg) {
-        let dom = this.__createAlertElement(msg, 'success');
+        let dom = this.__createPopupElement(msg, 'success', 'alert');
         let $dom = $(dom);
         $dom.hide();
         $("body").append($dom);
@@ -79,5 +97,60 @@ class AppPopups {
                 $dom.remove();
             });
         }, 6000);
+    }
+
+    __bindEvents($dom) {
+        $dom.find("#confirmButton").on("click", (e) => {
+            $(e.target).trigger("confirmed.bs.alert");
+            $(e.target).parents(".alert").alert('close');
+        });
+        $dom.find("#cancelButton").on("click", (e) => {
+            $(e.target).trigger("cancelled.bs.alert");
+            $(e.target).parents(".alert").alert('close');
+        });
+    }
+    confirmError(msg) {
+        let dom = this.__createPopupElement(msg, 'danger', 'confirm');
+        let $dom = $(dom);
+        $dom.hide();
+        $("body").append($dom);
+        $dom.fadeIn();
+
+        this.__bindEvents($dom);
+
+        return $dom;
+    }
+    confirmWarning(msg) {
+        let dom = this.__createPopupElement(msg, 'warning', 'confirm');
+        let $dom = $(dom);
+        $dom.hide();
+        $("body").append($dom);
+        $dom.fadeIn();
+
+        this.__bindEvents($dom);
+
+        return $dom;
+    }
+    confirmInfo(msg) {
+        let dom = this.__createPopupElement(msg, 'info', 'confirm');
+        let $dom = $(dom);
+        $dom.hide();
+        $("body").append($dom);
+        $dom.fadeIn();
+
+        this.__bindEvents($dom);
+
+        return $dom;
+    }
+    confirmSuccess(msg) {
+        let dom = this.__createPopupElement(msg, 'success', 'confirm');
+        let $dom = $(dom);
+        $dom.hide();
+        $("body").append($dom);
+        $dom.fadeIn();
+
+        this.__bindEvents($dom);
+
+        return $dom;
     }
 };
